ff32533df1e89fb89185fc33f77672723a750fb4
MarcoBousseau <bousseaum@free.fr>
2019-12-06 09:49:40 +0000

    06/12/19

--- bin/Principal.c
+++ bin/Principal.c
@@ -1,25 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void affiche_couche(int *couche, int taille){
+	int i;
+	for(i=0; i<taille; i++){
+		printf("%d", *(couche+i));
+	}
+}
+
 int main(void)
 {   
     //Creation de la matrice de coefficientset saisie des valeurs d'activation (4 valeurs pour nous)
     
-    int i, *nb_neurones_entree;
+    int i;
+    int nb_neurones_entree = 4;
     int *activation;
-    printf("Entrer le nombre de neurones que contient la couche d'entree");
-    scanf("%d", nb_neurones_entree);
-    activation = malloc((*nb_neurones_entree)*(sizeof(int)));
+    activation = malloc((nb_neurones_entree)*(sizeof(int)));
     printf("Entrer les valeurs d'activation");
-    for(i = 0; i < *nb_neurones_entree; i++){
+    for(i = 0; i < nb_neurones_entree; i++){
 		scanf("%d", activation+i);   	
     }
-    float *coefficients;
-    coefficients = malloc((*nb_neurones_entree)*(*nb_neurones_entree)*sizeof(float));
-    int j;
-    for(j = 0; j<(*nb_neurones_entree)*(*nb_neurones_entree); j++){
-		*(coefficients+j)=1;
-    }
+    
+    //Matrices de coefficients synaptiques 
+    
+    int *coeff1, *coeff2, *coeff3;
+    int nb_neurones_cache1 = 15;
+    int nb_neurones_cache2 = 5;
+    coeff1 = malloc((nb_neurones_cache1)*(nb_neurones_entree)*sizeof(int));
+    coeff2 = malloc((nb_neurones_cache2)*(nb_neurones_cache1)*sizeof(int));
+    coeff3 = malloc(1*(nb_neurones_cache2)*sizeof(int));
+    (*coeff1)[(nb_neurones_cache1)*(nb_neurones_entree)] = {1,1,1,1,1,0,1,0,0,1,0,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,1,1,0,0,0,1,1,0,0,0,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,1,1,1,1,0,1};
+    (*coeff2)[(nb_neurones_cache2)*(nb_neurones_cache1)] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1};
+	(*coeff3)[5] = {1,2,1,1,1};
     
     //Propagation : 1ere sous-couche
+    
+    int m;
+    int n;
+    int *activation_prem_couche;
+    activation_prem_couche = malloc((nb_neurones_cache1)*sizeof(int));
+    for(m = 0; m < (*nb_neurones_cache1); m++){
+    	for(n = 0; n<(nb_neurones_entree); n++){
+    		*(activation_prem_couche+m) = *(activation_prem_couche+m) + (*(activation + n + m*(nb_neurones_entree)));
+		}
+    }
+    affiche_couche(activation_prem_couche, nb_neurones_cache1);
+    //Propagation : 2 eme couche 
+    /*
+    int ;
+    int *activation_deux_couche;
+    activation_deux_couche = malloc(2*sizeof(int));
+    *(activation_deux_couche+0) = *(activation_prem_couche+0);
+	*(activation_deux_couche+1) = localisation(*(activation_prem_couche+0, *(activation_prem_couche+1), *(activation_prem_couche+2), *(activation_prem_couche+3), *(activation_prem_couche+4)), ;
+	
+	//Propagation : Couche de sortie
+	*/
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
